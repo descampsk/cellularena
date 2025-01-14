a0e65d86d853bce278b6420f54a970b26cd939f5
descampsk <22590202+descampsk@users.noreply.github.com>
2025-01-14 22:09:00 +0000

    feat: add sentry

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/vue'
+
 import { VueFire, VueFireAuth } from 'vuefire'
 import { firebaseApp } from './infra/firebase'
 
@@ -8,6 +10,19 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration({ router }), Sentry.replayIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', 'https://cellularena.web.app'],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 
 app.use(createPinia())
 app.use(router)
